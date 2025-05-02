2947b30cd249fa1fa4f31812b78394038515918e
Nadia <nadia.boujnah@laplateforme.io>
2025-05-02 14:26:28 +0200

    updates routes

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,50 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route vers la page d'accueil avec le composant React home.jsx
+
 Route::get('/', function () {
     return Inertia::render('home');
 })->name('home');
 
-// Routes protégées par authentification
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+
 Route::get('/activities', function () {
-    return Inertia::render('Activities');
+    return Inertia::render('activities');
 })->name('activities');
 
 
-// Autres fichiers de routes
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+Route::get('/activities/{id}', function ($id) {
+    return Inertia::render('details-activities', ['id' => (int) $id]);
+})->name('activity.details');
+
+Route::get('/about', function () {
+    return Inertia::render('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return Inertia::render('contact');
+})->name('contact');
+
+Route::get('/cgu', function () {
+    return Inertia::render('cgu');
+})->name('cgu');
+
+Route::get('/legal', function () {
+    return Inertia::render('legal');
+})->name('legal');
+
+Route::get('/privacy', function () {
+    return Inertia::render('privacy');
+})->name('privacy');
+
+
+
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
